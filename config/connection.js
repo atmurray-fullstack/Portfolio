4095933b6367c0d08904acef62cc6d2e5073f81b
Andrew Murray <atmurray@bellsouth.net>
2020-06-23 11:21:22 -0400

    port change

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('andrewPortfolio_db', 'root', 'root', {
     host: '127.0.0.1',
-    port: 3307,
+    port: 3306,
     dialect: 'mysql',
     pool: {
         max: 5,
