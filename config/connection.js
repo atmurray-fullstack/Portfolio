65b3cfe0a75d7d3eafe01d9197a60128bce0f968
Andrew Murray <atmurray@bellsouth.net>
2020-06-23 11:25:31 -0400

    added process.env.JAWSDB_URL to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('andrewPortfolio_db', 'root', 'root', {
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+if (process.env.JAWSDB_URL) {
+    const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
+} else {
+
+    const sequelize = new Sequelize('andrewPortfolio_db', 'root', 'root', {
+        host: '127.0.0.1',
+        port: 3306,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    });
+}
 module.exports = sequelize;
 
 
