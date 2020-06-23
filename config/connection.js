a4a1ffb4bb24165d4e58a205e80840eff3a60a23
Andrew Murray <atmurray@bellsouth.net>
2020-06-23 12:30:37 -0400

    added location.reoload to call to controller added env var and changed conditionals to connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 
     const sequelize = new Sequelize('andrewPortfolio_db', 'root', 'root', {
         host: '127.0.0.1',
-        port: 3306,
+        port: 3307,
         dialect: 'mysql',
         pool: {
             max: 5,
